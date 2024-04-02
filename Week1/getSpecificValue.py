a68f96fd26bcf354fe4ee988b9b25e4be19a5ae9
2Shashank <110035493+2Shashank@users.noreply.github.com>
2024-04-02 10:16:55 +0530

    Extract specific value from DB

--- Week1/getSpecificValue.py
+++ Week1/getSpecificValue.py
@@ -3,11 +3,7 @@ import etcd3
 def get_value_for_key(client, key):
     value = client.get(key)  # Get the value of the specified key
     
-    # Check if the key exists and return its value
-    if value is not None:
-        return value[0].decode()
-    else:
-        return None
+    return value[0].decode() # returning value
 
 def main():
     client = etcd3.client(host = "localhost" , port = 2379) # Connect to etcd server
@@ -16,10 +12,7 @@ def main():
 
     value = get_value_for_key(client, key) # Get value for the specified key
 
-    if value is not None:
-        print(f"The value of key '{key}' is: {value}") # Print the value of the key
-    else:
-        print(f"Key '{key}' not found.")
+    print(f"The value of key '{key}' is: {value}") # Print the value of the key
 
 if __name__ == "__main__":
     main()
