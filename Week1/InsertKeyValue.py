71013d4daa582a7c6801777e3fa195e984e251e8
sahil.kamate <117616880+skamate01@users.noreply.github.com>
2024-04-02 15:01:31 +0530

    Update InsertKeyValue.py

--- Week1/InsertKeyValue.py
+++ Week1/InsertKeyValue.py
@@ -1,18 +1,14 @@
 import etcd3
 
 def insert_to_etcd():
-    # Connect to etcd server
     etcd = etcd3.client(host='127.0.0.1', port=2379)
     
     while True:
-        # Prompt user for key and value
         key = input("Enter key (or 'exit' to stop): ")
         if key.lower() == 'exit':
             break
         
         value = input("Enter value: ")
-        
-        # Insert key-value pair into etcd
         etcd.put(key, value)
         print(f"Inserted: {key} => {value}")
 
